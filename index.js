1c296a6aecc38cbb9bc2833522c66cbd3687b292
Camden Whirl <camdenwhirl@gmail.com>
2021-07-02 14:34:06 -0600

    email validation and employee object constructor

--- index.js
+++ index.js
@@ -1,71 +1,122 @@
 const EMPLOYEE = require('./lib/employee');
 const MANAGER = require ('./lib/manager');
-const ENGINEER = require('./lib/engi');
+const ENGI = require('./lib/engi');
 const INTERN = require('./lib/intern');
 const path = require('path');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+let lcv;
+let team = [];
+const namePrompt = `
+{
+    name:"employeeName",
+    type: "input",
+    message:"What is the employee's name?",
+    validate: userIn => {
+        if (userIn !== '') {
+            return true;
+        }
+        return "Please enter the team employee's name";
+    }
+}`;
+const emailPrompt = `
+{
+    name:"employeeEmail",
+    type: "input",
+    message: "What is the employee's email?",
+    validate: userIn => {
+        if(validateEmail(userIn)){
+            return true;
+        }
+        return "Invalid Email";
+    }
+}`;
+const idPrompt = `
+{
+    name:"employeeID",
+    type: "input",
+    message:"What is their ID number?",
+    validate: userIn => {
+        if (!isNaN(userIn)){
+            return true;
+        }
+        return "Please enter only numeric values";
+    }
+}`;
+const gitPrompt = `
+{
+    name:"employeeGit",
+    type: "input",
+    message:"What is their github username?",
+}`;
+const officePrompt = `
+{
+    name:"employeeOffice",
+    type: "input",
+    message:"What is their office number?",
+    validate: userIn => {
+        if (!isNaN(userIn)){
+            return true;
+        }
+        return "please enter the employee's office number";
+    }
+}`;
+const schoolPrompt = `
+{
+    name:"employeeSchool",
+    type: "input",
+    message:"What is their school's name?",
+    validate: userIn => {
+        if (!isNaN(userIn)){
+            return true;
+        }
+        return "please enter the employee's school name";
+    }
+}`;
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 };
 
-function inquireManager() {
-    inquirer.prompt([
-        {
-            name:"managerName",
-            type: "input",
-            message:"What is the manager's name?",
-            validate: userIn => {
-                if (userIn !== '') {
-                    return true;
-                }
-                return "Please enter the team manager's name";
-            }
-        },
-        {
-            name:"managerID",
-            type: "input",
-            message:"What is their ID number?",
-            validate: userIn => {
-                if (!isNaN(userIn)){
-                    return true;
-                }
-                return "Please enter only numeric values";
-            }
-        },
-        {
-            name:"managerEmail",
-            type: "input",
-            message: "What is the manager's email?",
-            validate: userIn => {
-                if(validateEmail(userIn)){
-                    return true;
-                }
-                return "Invalid Email";
-            }
-        },
-        {
-            name: "managerOffice",
-            type: "input",
-            message: "What is the manager's office number?",
-            validate: userIn => {
-                if(!isNaN(userIn)) {
-                    return true;
-                }
-                return "Please enter only numeric values";
-            }
+function buildQuestions(role) {
+    switch(role) {
+        case "manager":
+            return `${namePrompt},${emailPrompt},${idPrompt},${officePrompt}`;
+        case "engi":
+            return `${namePrompt},${emailPrompt},${idPrompt},${gitPrompt}`;
+        case "intern":
+            return `${namePrompt},${emailPrompt},${idPrompt},${schoolPrompt}`;
+    };
+}
+
+function buildEmpoyee(role) {
+    inquirer.prompt(buildQuestions(role)).then(answers => {
+        switch(role) {
+            case engi:
+                const employee = new ENGI(
+                    answers.employeeName,
+                    answers.employeeEmail,
+                    answers.employeeID,
+                    answers.employeeGit
+                );
+                team.push(employee);
+                break;
+            case intern:
+                const employee = new INTERN()
+                break;
+            case manager:
+                const employee = new MANAGER()
+                break;
         }
-    ]).then(answers => {
-        const manager = new MANAGER(
-            answers.managerName,
-            answers.managerId,
-            answers.managerEmail,
-            answers.managerOffice,
-        )
-        console.log(manager);
     });
 };
 
-inquireManager();
+
+
+// do {
+
+
+//   } while (lcv != "exit");
+
+console.log(buildQuestions("engi"));
